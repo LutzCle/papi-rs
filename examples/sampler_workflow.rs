26012bf37a8aa8cde49e5d5867abe4344c9637fb
Clemens Lutz <lutzcle@cml.li>
2018-10-01 12:52:41 +0200

    Add Sampler workflow example

---
+++ examples/sampler_workflow.rs
@@ -0,0 +1,30 @@
+extern crate papi;
+
+fn main() {
+
+    let papi = papi::Papi::init().unwrap();
+    let sampler = papi::sampler::SamplerBuilder::new(&papi)
+        .add_event("CPU_CLK_UNHALTED")
+        .unwrap()
+        .build()
+        .start()
+        .unwrap();
+
+    // Do some work
+    work();
+
+    let sample = sampler.stop().unwrap();
+    println!("CPU_CLK_UNHALTED: {}", sample);
+
+}
+
+fn work() {
+
+    let collected: u32 = (0..100)
+        .map(|x| x * 2)
+        .filter(|x| x % 3 == 0)
+        .sum();
+
+    println!("Summed up {}", collected);
+
+}
